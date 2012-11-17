dcc3c6c6f6caac4b59cfba6ebbaed24327ebf5d6
Pavel Kartavyy <p.kartaviy@gmail.com>
2012-11-17 17:29:23 +0400

    Modified EthernetServer and client for ability to use as serial

--- Cura/wicron/ethernetAdapter.py
+++ Cura/wicron/ethernetAdapter.py
@@ -1,23 +1,46 @@
 import __init__
+
 import socket
+from util.machineCom import VirtualPrinter
+from threading import Thread
+
+# The goal of this ethernet adapter to have the same interface and 
+# behaviout as a printer connected by serial port.
+
+def getMyIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
+    ip = ''
+    try:
+        s.connect(('8.8.8.8', 80));
+        ip = s.getsockname()[0];
+    except socket.error as e:
+        ip = 'localhost'
+        
+    s.close()
+    return ip
 
 class EthernetClient(socket.socket):
-    def __init__(self, _socket = None):
+    def __init__(self, _socket = None, timeout = 0.1):
         socket.socket.__init__(self, socket.AF_INET, socket.SOCK_STREAM, proto=0, _sock=_socket)
         self.inMessage = ''
+        self.settimeout(timeout)
         
     def write(self, data):
         self.sendall(data)
   
+    # return '' if no data was read before timeout
+    # in case of errors throws exception
     def readline(self):
         returnPos = -1
         while returnPos == -1:
-            income = self.recv(4096)
-            if not len(income):
-                # I decided not to return inMessage 
-                # because it is damaged - at least threre is no \n
-                # at the end of string
+            income = ''
+            try:
+                income = self.recv(4096)
+            except socket.timeout:
+                #we ignore timeout and return ''
                 return ''
+            if not len(income):
+                raise socket.error
             self.inMessage += income
             returnPos = self.inMessage.find("\n")
         result = self.inMessage[:returnPos+1]
@@ -25,29 +48,90 @@ class EthernetClient(socket.socket):
         return result
 
 class  EthernetServer:
-    def __init__(self, port):
-        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, port, timeout = 0.1):
+        # We use multithreading because we have one general resource - printer,
+        # which is used in every thread
+        # In CPython only one thread can be run simulteneously
+        # This causes perfomancy problems but it usually doesn't make sense in IO tasks
+        # If you don't have general resource and you want better perfomance than
+        # it is better for you to use multiprocessing.
         self.port = port
-    
-    def start(self):
-        host = ''
-        # There is some problem in detection of your ip
-        # when DNS server is not properly configured
-        # http://mail.python.org/pipermail/python-bugs-list/2004-May/022890.html
-        try:
-            host = socket.gethostbyname(socket.gethostname())
-        except:
-            host = ''
+        self.creatorThread = None
+        self.serversocket = None
+        self.enableWork = False
+        self.timeout = timeout
         
-        self.serversocket.bind((host, self.port))
-        self.serversocket.listen(5)
-        
-        (clientSocket, address) = self.serversocket.accept()
-        s = EthernetClient(_socket = clientSocket)
-        
-        result = "dummy"
-        while len(result)>0:
-            result = s.readline()
-            s.write(result)
+    def __del__(self):
+        self.stop()
 
+    def start(self):
+        host = getMyIp()
+        if( not self.creatorThread):
+            self.enableWork = True
+            
+            self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.serversocket.bind((host, self.port))
+            self.serversocket.listen(5)
+            self.serversocket.settimeout(self.timeout)
+            
+            self.creatorThread = Thread(target = self.__creator__)
+            self.creatorThread.start()
+
+    def stop(self):
+        self.enableWork = False
+            
+        if (self.creatorThread):
+            self.creatorThread.join()
+            self.creatorThread = None
+        
+        self.serversocket.close()
+        
+
+    def __creator__(self):
+        host = getMyIp()
+        
+        socketThreades = []
+        while(self.enableWork):
+            try:
+                (clientSocket, address) = self.serversocket.accept()
+            except:
+                #timeout
+                continue
+            else:    
+                s = EthernetClient(_socket = clientSocket)
+
+                socketThreades.append(Thread(target = self.__processSocket__, args=(s,)))
+                socketThreades[-1].start()
+        
+        for process in socketThreades:
+            process.join()
+
+    def __processSocket__(self, _socket):
+        _socket.settimeout(self.timeout)
+        socketWorking = True
+        while ( socketWorking and self.enableWork):
+            inp = ''
+            try:
+                inp = _socket.readline()
+            except:
+                socketWorking = False
+
+            if socketWorking:
+                out = self.processData(inp)
+                if len(out):
+                    _socket.write(out)
+        _socket.close()
+        
+    # Should process input string from client and return output to client
+    # If output contains no elementh than it won't be sent 
+    def processData(self, inp):
+        return inp
+
+class PrintServer(EthernetServer):
+    def __init__(self, port, printer=VirtualPrinter()):
+        EthernetServer.__init__(self, port)
+        self.printer = printer
     
+    def processData(self, inp):
+        self.printer.write(inp)
+        return self.printer.readline()
--- Cura/wicron/testEthernetAdapter.py
+++ Cura/wicron/testEthernetAdapter.py
@@ -1,3 +1,9 @@
+# Add needed parent folders to path
+import sys
+sys.path.append('../')
+
+import __init__
+
 import unittest
 import time
 from ethernetAdapter import *
@@ -5,62 +11,122 @@ from multiprocessing import Process
 import string
 import random
 
-TEST_HOST = "localhost"
+TEST_HOST = getMyIp()
 TEST_PORT = 3000
 
+def start(host, port, server = None, client = None):
+   if (not client):
+       client = EthernetClient()
+   if (not server):
+       server = EthernetServer(port)
+   try:
+       server.start()
+   except Exception as e :
+       print "Failed to start server \n", e 
+       return (False, None, None)
+        
+   # Try to connect to server
+   # We have to wait until server starts
+   attempts = 50
+   delay = 0.02
+   while attempts>0:
+       attempts-=1
+       try:
+           client.connect((host, port))
+       except:
+           time.sleep(delay) 
+       else:
+           break
+
+   if(attempts == 0):
+       print "Failed to connect to server"
+       return (False, None, None)
+   return (True, server, client)
+
+def stop(server, client):
+    if(client):
+        client.close()
+    if(server):
+        server.stop()
 class TestEthernetAdapter(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print "Setup class TestEthernetAdapter"
-        cls.server = EthernetServer(TEST_PORT)
-        cls.process = Process(target = cls.server.start)
-        cls.process.start()
-        cls.client = EthernetClient()
-        
-        # Try to connect to server
-        # We have to wait until server starts
-        attempts = 50
-        delay = 0.01
-        while attempts>0:
-            attempts-=1
-            try:
-                cls.client.connect((TEST_HOST, TEST_PORT))
-            except:
-                time.sleep(delay)
-            else:
-                break
-        cls.succesfulConection =  (attempts > 0)
+        print "Test client server communication"
+        print "Server will be run on this pc with IP:PORT=", str(TEST_HOST) + ":" + str(TEST_PORT)
+        (cls.successfulConnection , cls.server, cls.client) = start(TEST_HOST, TEST_PORT)
 
     @classmethod
     def tearDownClass(cls):
-        cls.client.close()
-        cls.process.join()
-        print "Tear down class TestEthernetAdapter"
+        stop(cls.server, cls.client)
 
     def testConnection(self):
-        self.assertTrue(TestEthernetAdapter.succesfulConection)        
+        self.assertTrue(TestEthernetAdapter.successfulConnection)
+   
     def testTransmission(self):
+        if(not TestEthernetAdapter.successfulConnection):
+            self.skipTest("Failed to connect to server")
+
         counter = 5
         while counter>0:
             counter-=1
             msg = "Hello, friend\n"
-            print "Client: " + msg
             TestEthernetAdapter.client.write(msg)
             answer = self.client.readline()
             self.assertTrue(len(answer)>0)
-            print "Server: " + answer
-    
+         
     def testLongTransmissions(self):
+        if(not TestEthernetAdapter.successfulConnection):
+            self.skipTest("Failed to connect to server")
+
         num = 10000
         msg = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(num))
         msg += '\n'
 
-        print "write big line"
         TestEthernetAdapter.client.write(msg)
-        print "read big line"
         answer = TestEthernetAdapter.client.readline()
         
-        self.assertEqual(msg, answer) 
+        self.assertEqual(msg, answer)
+        
+    def testMultiClients(self):
+        if(not TestEthernetAdapter.successfulConnection):
+            self.skipTest("Failed to connect to server")
 
+        N = 100
+        clients = [ EthernetClient() for c in range(0, N)]
+        
+        testMsg = "Test input \n"
+        for client in clients:
+            client.connect((TEST_HOST, TEST_PORT))
+            client.write(testMsg)
+            out  = client.readline()
+            self.assertEqual(testMsg, out)
+
+class TestPrintServer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        (cls.connected, cls.server, cls.client) = start(TEST_HOST, TEST_PORT, server = PrintServer(TEST_PORT))
+        cls.virtualPrinter = VirtualPrinter()
+
+    @classmethod
+    def tearDownClass(cls):
+        stop(cls.server, cls.client)
+     
+    def testVsVirtualPrinter(self):
+        if(not TestPrintServer.connected):
+            self.skipTest("Failed to connect to server")
+        for i in xrange(0, 5):
+            vp = self.virtualPrinter.readline()
+            if vp =='':
+                break
+            
+            rvp = ''
+            while rvp=='':
+                rvp = self.client.readline()
+            self.assertEqual(vp, rvp)
+        
+        for i in xrange(0, 100):
+            print self.virtualPrinter.readline()
+
+    
 if __name__ == '__main__':
     unittest.main()
